c545d5f031e4ead5b9099e0360de70368a903561
ISB-021Fulcrum <79363899+ISB-021Fulcrum@users.noreply.github.com>
2025-03-08 01:22:52 +0200

    Add files via upload

---
+++ s3_website/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure AWS provider and creds
+provider "aws" {
+  region                   = "us-east-1"
+  shared_config_files      = [""]
+  shared_credentials_files = [""]
+  profile                  = ""
+}
+
+# Creating bucket
+resource "aws_s3_bucket" "website" {
+  bucket = "gracelesstarnishedemboldenedbytheflameofambition"
+
+  tags = {
+    Name        = "Website"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "website_config" {
+  bucket = aws_s3_bucket.website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_object" "indexfile" {
+  bucket       = aws_s3_bucket.website.id
+  key          = "index.html"
+  source       = "./src/index.html"
+  content_type = "text/html"
+}
+
+output "website_endpoint" {
+  value = aws_s3_bucket_website_configuration.website_config.website_endpoint
+}
